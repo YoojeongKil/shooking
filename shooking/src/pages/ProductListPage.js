1f86cdbac7faf330587317bccba8ddd2d3d696d9
YoojeongKil <81125777+YoojeongKil@users.noreply.github.com>
2025-08-08 02:45:25 +0900

    Update ProductListPage.js

--- shooking/src/pages/ProductListPage.js
+++ shooking/src/pages/ProductListPage.js
@@ -1,20 +1,14 @@
-import { useState } from "react";
 import Header from "../components/Header";
 import ProductList from "../components/ProductList";
 import products from "../data/products";
 import { useNavigate } from "react-router-dom";
 
 export default function ProductListPage() {
-  const [cartCount, setCartCount] = useState(0);
   const navigate = useNavigate();
 
   return (
     <div>
-      <Header
-        showCartButton={true}
-        cartCount={cartCount}
-        onCartClick={() => navigate("/cart")}
-      />
+      <Header showCartButton={true} onCartClick={() => navigate("/cart")} />
       <div className="p-5">
         <div className="mb-5">
           <div className="text-[30px] font-extrabold">신발 상품 목록</div>
@@ -22,7 +16,7 @@ export default function ProductListPage() {
             현재 {products.length}개의 상품이 있습니다.
           </div>
         </div>
-        <ProductList products={products} setCartCount={setCartCount} />
+        <ProductList products={products} />
       </div>
     </div>
   );
